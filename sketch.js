9a25f8cde9a729eb8b6b70270cede37724b9f55b
Joshua <dendauwj@oregonstate.edu>
2017-10-08 11:49:29 -0700

    circle moves across and wraps around the screen

--- sketch.js
+++ sketch.js
@@ -1,12 +1,24 @@
 // Zombulator by Joshua Dendauw
 
+var zombieX = 50;
+var humanX = 100;
+
 function setup() {
 	createCanvas(800, 800);
 }
 
 function draw() {
-	fill(255, 0, 0);
-	ellipse(50, 50, 80, 80);
-	fill(150, 150, 200);
-	ellipse(200, 100, 80, 80);
+	background(255, 255, 255);
+	fill(148, 181, 1);
+	ellipse(zombieX, 50, 80, 80);
+	fill(0, 150, 200);
+	ellipse(humanX, 100, 80, 80);
+	zombieX = zombieX + 1;
+	humanX = humanX + 3;
+	if (zombieX >= 800) {
+		zombieX = 0;
+	}
+	if (humanX >= 800) {
+		humanX = 0;
+	}
 }
