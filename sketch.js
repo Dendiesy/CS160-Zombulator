7dba093be41991803851e8e8e12346d988e99d87
Joshua <dendauwj@oregonstate.edu>
2017-10-10 22:53:09 -0700

    Human and Zombie circles drop and bounce

--- sketch.js
+++ sketch.js
@@ -1,24 +1,43 @@
 // Zombulator by Joshua Dendauw
 
-var zombieX = 50;
-var humanX = 100;
+var zombieY = 50;
+var zombieV = 0;
+var zombieA = 0.2;
+var zombieDamping = -0.8;
+var zombieSize = 40;
+var zombieColor;
+var humanY = 50;
+var humanV = 0;
+var humanA = 0.2;
+var humanDamping = -0.8;
+var humanSize = 40;
+var humanColor;
+var backgroundColor;
 
 function setup() {
-	createCanvas(800, 800);
+	createCanvas(windowWidth, windowHeight);
+	backgroundColor = color(0, 75, 95);
+	zombieColor = color(50, 255, 120);
+	humanColor = color(50, 120, 255);
 }
 
 function draw() {
-	background(255, 255, 255);
-	fill(148, 181, 1);
-	ellipse(zombieX, 50, 80, 80);
-	fill(0, 150, 200);
-	ellipse(humanX, 100, 80, 80);
-	zombieX = zombieX + 1;
-	humanX = humanX + 3;
-	if (zombieX >= 800) {
-		zombieX = 0;
+	background(backgroundColor);
+	noStroke();
+	fill(zombieColor);
+	ellipse(windowWidth / 2, zombieY, zombieSize, zombieSize);
+	zombieY += zombieV;
+	zombieV += zombieA;
+	if (zombieY + (zombieSize / 2) >= windowHeight) {
+		zombieY = windowHeight - (zombieSize / 2);
+		zombieV *= zombieDamping;
 	}
-	if (humanX >= 800) {
-		humanX = 0;
+	fill(humanColor);
+	ellipse(windowWidth / 3, humanY, humanSize, humanSize);
+	humanY += humanV;
+	humanV += humanA;
+	if (humanY + (humanSize / 2) >= windowHeight) {
+		humanY = windowHeight - (humanSize / 2);
+		humanV *= humanDamping;
 	}
 }
