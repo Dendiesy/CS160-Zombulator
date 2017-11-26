fc4cfbe62660b0860ba1a74474a37ca3a4b784a9
Joshua <dendauwj@oregonstate.edu>
2017-11-26 14:03:04 -0800

    Collision detection

--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
 // Zombulator by Joshua Dendauw
-// CS 160 Exercise 16: Biased Random Walk
+// CS 160 Exercise 20: Collisions
 
 var backgroundColor;
 
 const MIN_SIZE = 5;
-const MAX_SIZE = 50;
+const MAX_SIZE = 25;
 const POPULATION_SIZE = 500;
 
 var population = [];
 
+var zombieCount = 0;
+var humanCount = 0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  backgroundColor = color(245, 255, 245);
+  backgroundColor = color(50, 111, 50);
   initializePopulation();
 }
 
@@ -20,36 +23,63 @@ function draw() {
   noStroke();
   drawPopulation();
   movePopulation();
+  drawPopulationCounts();
+  handleCollisions();
 }
 
-function initializePopulation() {
+function handleCollisions() {
 	for (var i = 0; i < POPULATION_SIZE; ++i) {
-		var humanoid_type = random(0, 100)
-		if (humanoid_type <= 50) {
-			population[i] = initializeZombie();
-		} else {
-			population[i] = initializeHuman();
+		var attacker = population[i];
+		for (var j = i + 1; j < POPULATION_SIZE; ++j) {
+			var target = population[j];
+			if (attacker.isTouching(target)) {
+				print("Fight! Fight! Fight!");
+
+			}
+
 		}
 	}
 }
 
+function initializePopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    var humanoid_type = random(0, 100);
+    if (humanoid_type <= 50) {
+      population[i] = initializeZombie();
+      ++zombieCount;
+    } else {
+      population[i] = initializeHuman();
+      ++humanCount;
+    }
+  }
+}
+
+function drawPopulationCounts() {
+  stroke(0);
+  textSize(48);
+  textAlign(CENTER);
+  text("Zombies: " + zombieCount, width / 2, 100);
+  text("Humans: " + humanCount, width / 2, height - 100);
+}
+
 function drawPopulation() {
-	for (var i = 0; i < POPULATION_SIZE; ++i) {
-		population[i].draw();
-	}
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    population[i].draw();
+  }
 }
 
 function movePopulation() {
-	for (var i = 0; i < POPULATION_SIZE; ++i) {
-		population[i].move();
-	}
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    population[i].move();
+  }
 }
 
 function initializeZombie() {
   return {
+  	humanoidType: "zombie",
     x: random(0, windowWidth),
     y: random(0, 200),
-    speed: random(0.25, 3),
+    speed: random(0.75, 3),
     size: random(MIN_SIZE, MAX_SIZE),
     color: color(random(50, 150), random(100, 255), random(50, 150), 150),
     move: function() {
@@ -67,32 +97,43 @@ function initializeZombie() {
     draw: function() {
       fill(this.color);
       ellipse(this.x, this.y, this.size, this.size);
+    },
+    isTouching: function(target) {
+      if (this.humanoidType == target.humanoidType) return false;
+      var distance = dist(this.x, this.y, target.x, target.y);
+      return distance <= (this.size/2 + target.size/2);
     }
   };
 }
 
 function initializeHuman() {
   return {
+  	humanoidType: "human",
     x: random(0, windowWidth),
     y: random(windowHeight - 200, windowHeight),
-    speed: random(0.25, 3),
+    speed: random(0.75, 3),
     size: random(MIN_SIZE, MAX_SIZE),
     color: color(random(50, 150), random(50, 150), random(150, 255), 150),
     move: function() {
-    	var direction = random(0, 100);
-    	if (direction < 20) {
-    		this.x += this.speed;
-    	} else if (direction < 40) {
-    		this.x -= this.speed;
-    	} else if (direction < 60) {
-    		this.y += this.speed;
-    	} else {
-    		this.y -= this.speed;
-    	}
-    },
+        var direction = random(0, 100);
+        if (direction < 20) {
+          this.x += this.speed;
+        } else if (direction < 40) {
+          this.x -= this.speed;
+        } else if (direction < 60) {
+          this.y += this.speed;
+        } else {
+          this.y -= this.speed;
+        }
+      },
     draw: function() {
-    	fill(this.color);
-    	ellipse(this.x, this.y, this.size, this.size);
+        fill(this.color);
+        ellipse(this.x, this.y, this.size, this.size);
+    },
+    isTouching: function(target) {
+      if (this.humanoidType == target.humanoidType) return false;
+      var distance = dist(this.x, this.y, target.x, target.y);
+      return distance <= (this.size/2 + target.size/2);
     }
   };
 }
